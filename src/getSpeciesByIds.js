e13448677549e46f36130814937d893e93539ea4
Ruan Chagas <ruanchagas95@gmail.com>
2023-06-14 19:20:04 -0300

    6 - complete

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,6 +3,5 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 const getSpeciesByIds = (...ids) => species.filter((specie) => ids.includes(specie.id));
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,19 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('verifica se ao passar o parâmetro count retorna a quantidade de elefantes', () =>
+    expect(handlerElephants('count')).toBe(4));
+  it('verifica se ao passar o parâmetro names retorna um array que inclui Jefferson', () =>
+    expect(handlerElephants('names').includes('Jefferson')).toBeTruthy());
+  it('verifica se ao passar o parâmetro averageAge retorna um número próximo a 10.5', () =>
+    expect(handlerElephants('averageAge') > 10 && handlerElephants('averageAge') < 11)
+      .toBeTruthy());
+  it('verifica se ao passar o parâmetro location retorna a localização dos elefantes dentro do zoológico', () =>
+    expect(handlerElephants('location')).toBe('NW'));
+  it('verifica se ao passar o parâmetro popularity retorna um número igual ou maior a 5', () =>
+    expect(handlerElephants('popularity') >= 5).toBeTruthy());
+  it('verifica se ao passar o parâmetro availability retorna um número igual ou maior a 5', () =>
+    expect(handlerElephants('availability').includes('Monday')).toBeFalsy());
+  it('Não passando argumentos a função deve retornar undefined', () =>
+    expect(handlerElephants()).toBeUndefined());
+});
