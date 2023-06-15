629174c6c881144f8a7798e33926282c3223e8e4
Ruan Chagas <ruanchagas95@gmail.com>
2023-06-15 18:16:54 -0300

    9 - complete

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -4,16 +4,20 @@ const getSchedule = (scheduleTarget) => {
   if (species.find((specieCurrent) => specieCurrent.name === scheduleTarget)) {
     return species.find((specieCurrent) => specieCurrent.name === scheduleTarget).availability;
   }
-  if (hours[scheduleTarget]) {
-    return { [scheduleTarget]: { exhibition: species.filter((specie) => specie.availability
-      .find((day) => day === scheduleTarget))
-      .map((specie) => specie.name),
-    officeHour:
-      `Open from ${hours[scheduleTarget].open}am until ${hours[scheduleTarget].close}pm`,
+  const createDay = (day) => {
+    const speciesList = species
+      .filter((specie) => specie.availability.includes(day))
+      .map((specie) => specie.name);
+    return { [day]: { exhibition: speciesList.length ? speciesList : 'The zoo will be closed!',
+      officeHour:
+      hours[day].open ? `Open from ${hours[day].open}am until ${hours[day].close}pm` : 'CLOSED',
     },
     };
+  };
+  if (hours[scheduleTarget]) {
+    return createDay(scheduleTarget);
   }
+  return Object.keys(hours).reduce((obj, day) => ({ ...obj, ...createDay(day) }), {});
 };
 
-console.log(getSchedule('lins'));
 module.exports = getSchedule;
