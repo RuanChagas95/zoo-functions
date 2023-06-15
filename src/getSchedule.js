bf3ef89996438973aa52389ca9d2b55b407138af
Ruan Chagas <ruanchagas95@gmail.com>
2023-06-15 16:59:33 -0300

    9 - chore

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,19 @@
-const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
 
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  if (species.find((specieCurrent) => specieCurrent.name === scheduleTarget)) {
+    return species.find((specieCurrent) => specieCurrent.name === scheduleTarget).availability;
+  }
+  if (hours[scheduleTarget]) {
+    return { [scheduleTarget]: { exhibition: species.filter((specie) => specie.availability
+      .find((day) => day === scheduleTarget))
+      .map((specie) => specie.name),
+    officeHour:
+      `Open from ${hours[scheduleTarget].open}am until ${hours[scheduleTarget].close}pm`,
+    },
+    };
+  }
 };
 
+console.log(getSchedule('lins'));
 module.exports = getSchedule;
