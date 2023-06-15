2ebbb80d6c6d5caf2c327c1197a755dfdaf3b57d
Ruan Chagas <ruanchagas95@gmail.com>
2023-06-15 14:36:47 -0300

    8 - complete

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
-};
+const countEntrants = (entrants) => (entrants.reduce((count, entrant) => {
+  let ageGroup;
+  if (entrant.age < 18) {
+    ageGroup = 'child';
+  } else {
+    ageGroup = entrant.age >= 50 ? 'senior' : 'adult';
+  }
+  return { ...count, [ageGroup]: (count[ageGroup] || 0) + 1 };
+}, {})
+);
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (entrants) {
+    const count = countEntrants(entrants);
+    return Object.keys(count)
+      .reduce((acc, key) => (acc + (count[key] * prices[key])), 0) || 0;
+  }
+  return 0;
 };
-
 module.exports = { calculateEntry, countEntrants };
