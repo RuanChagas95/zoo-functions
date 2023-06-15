82be944232f3d1152763974a1c2093646de05c73
Ruan Chagas <ruanchagas95@gmail.com>
2023-06-15 19:49:35 -0300

    9 - complete

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,19 +1,20 @@
 const { species, hours } = require('../data/zoo_data');
 
+const createDay = (day) => {
+  const speciesList = species
+    .filter((specie) => specie.availability.includes(day))
+    .map((specie) => specie.name);
+  return { [day]: { exhibition: speciesList.length ? speciesList : 'The zoo will be closed!',
+    officeHour:
+    hours[day].open ? `Open from ${hours[day].open}am until ${hours[day].close}pm` : 'CLOSED',
+  },
+  };
+};
 const getSchedule = (scheduleTarget) => {
   if (species.find((specieCurrent) => specieCurrent.name === scheduleTarget)) {
     return species.find((specieCurrent) => specieCurrent.name === scheduleTarget).availability;
   }
-  const createDay = (day) => {
-    const speciesList = species
-      .filter((specie) => specie.availability.includes(day))
-      .map((specie) => specie.name);
-    return { [day]: { exhibition: speciesList.length ? speciesList : 'The zoo will be closed!',
-      officeHour:
-      hours[day].open ? `Open from ${hours[day].open}am until ${hours[day].close}pm` : 'CLOSED',
-    },
-    };
-  };
+
   if (hours[scheduleTarget]) {
     return createDay(scheduleTarget);
   }
