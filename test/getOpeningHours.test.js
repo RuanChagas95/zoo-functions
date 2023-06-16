0c5f414829332cd2edad64b8930f373315f26996
Ruan Chagas <ruanchagas95@gmail.com>
2023-06-16 16:32:33 -0300

    13 complete

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,27 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+const obj = {
+  Tuesday: { open: 8, close: 6 },
+  Wednesday: { open: 8, close: 6 },
+  Thursday: { open: 10, close: 8 },
+  Friday: { open: 10, close: 8 },
+  Saturday: { open: 8, close: 10 },
+  Sunday: { open: 8, close: 8 },
+  Monday: { open: 0, close: 0 },
+};
+describe('Testes da função getOpeningHours', () => {
+  it('Para os argumentos Monday e 09:00-AM deve retornar a string \'The zoo is closed\'', () =>
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed'));
+  it('A função sem argumentos deverá retornar um obj com o cronograma de horários', () =>
+    expect(getOpeningHours()).toEqual(obj));
+  it('Para os argumentos Tuesday e 10:00-AM deve retornar a string \'The zoo is open\'', () =>
+    expect(getOpeningHours('Tuesday', '10:00-AM')).toBe('The zoo is open'));
+  it('Para os argumentos Wednesday e 11:00-PM deve retornar a string \'The zoo is closed\'', () =>
+    expect(getOpeningHours('Wednesday', '11:00-PM')).toBe('The zoo is closed'));
+  it('Para os argumentos Thu e 09:00-AM deve lançar uma exceção com a mensagem: \'The day must be valid. Example: Monday\'', () =>
+    expect(() => { getOpeningHours('Thu', '09:00-AM'); }).toThrow(/Monday/));
+  it('Para os argumentos Thu e 09:00-AM deve lançar uma exceção com a mensagem: \'The day must be valid. Example: Monday\'', () =>
+    expect(() => { getOpeningHours('Friday', '09:00-ZM'); }).toThrow(/abbreviation/));
+  it('Para os argumentos Sunday e 09:c0-AM deve lançar uma exceção com a mensagem: \'The minutes should represent a number\'', () =>
+    expect(() => { getOpeningHours('Sunday', '09:c0-AM'); }).toThrow(/minutes/));
+});
