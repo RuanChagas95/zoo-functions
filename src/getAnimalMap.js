278922fbab87fa69697fba5ea2d1f444b5aa434e
Ruan Chagas <ruanchagas95@gmail.com>
2023-06-18 17:43:16 -0300

    chore 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const getAnimalMap = (options) => {
-  // seu código aqui
+const locations = { NE: [], NW: [], SE: [], SW: [] };
+const getAnimalMap = (options = {}) => {
+  const map = species.reduce((obj, specie) => ({ ...obj,
+    [specie.location]: [...obj[specie.location],
+      specie.name] }), locations);
+  if (options.includeNames) {
+    return Object.entries(map).reduce((obj, entry) => {
+      const speciesResidents = entry[1].map((specieName) => ({ [specieName]: species
+        .find((specie) => specie.name === specieName)
+        .residents.filter((resident) => resident.sex === (options.sex || resident.sex))
+        .map((resident) => resident.name) }));
+      return { ...obj, [entry[0]]: speciesResidents };
+    },
+    {});
+  }
+  return map;
 };
-
+console.log(getAnimalMap({ includeNames: true }));
 module.exports = getAnimalMap;
