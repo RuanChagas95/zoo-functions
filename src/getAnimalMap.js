43212537686099f498a40ef40ab35e2b3fac2dab
Ruan Chagas <ruanchagas95@gmail.com>
2023-06-18 17:49:47 -0300

    refact 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,19 +1,25 @@
 const { species } = require('../data/zoo_data');
 
 const locations = { NE: [], NW: [], SE: [], SW: [] };
+function refactMap(map, options) {
+  return Object.entries(map).reduce((obj, entry) => {
+    const speciesResidents = entry[1].map((specieName) => {
+      const residents = species
+        .find((specie) => specie.name === specieName)
+        .residents.filter((resident) => resident.sex === (options.sex || resident.sex))
+        .map((resident) => resident.name);
+      return { [specieName]: residents };
+    });
+    return { ...obj, [entry[0]]: speciesResidents };
+  },
+  {});
+}
 const getAnimalMap = (options = {}) => {
   const map = species.reduce((obj, specie) => ({ ...obj,
     [specie.location]: [...obj[specie.location],
       specie.name] }), locations);
   if (options.includeNames) {
-    return Object.entries(map).reduce((obj, entry) => {
-      const speciesResidents = entry[1].map((specieName) => ({ [specieName]: species
-        .find((specie) => specie.name === specieName)
-        .residents.filter((resident) => resident.sex === (options.sex || resident.sex))
-        .map((resident) => resident.name) }));
-      return { ...obj, [entry[0]]: speciesResidents };
-    },
-    {});
+    return refactMap(map, options);
   }
   return map;
 };
