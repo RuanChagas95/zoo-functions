26e318de2c05338f7a220b5199a175b753d86c62
Ruan Chagas <ruanchagas95@gmail.com>
2023-06-18 15:05:38 -0300

    11 - complete

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,23 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const getEmployeesCoverage = (employeePart) => {
+  if (!employeePart) {
+    return employees.map((employee) => getEmployeesCoverage(employee));
+  }
+  try {
+    const employee = employees.find((employeeCurr) => (employeeCurr.id === employeePart.id
+      || employeeCurr.firstName === employeePart.name || employeeCurr.lastName === employeePart.name));
+    const employeeSpecies = employee.responsibleFor.map((id) =>
+      species.find((specie) => specie.id === id).name);
+    const speciesLocations = employee.responsibleFor.map((id) =>
+      species.find((specie) => specie.id === id).location);
+    return { id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: employeeSpecies,
+      locations: speciesLocations,
+    };
+  } catch (error) {
+    throw new Error('Informações inválidas');
+  }
 };
-
 module.exports = getEmployeesCoverage;
