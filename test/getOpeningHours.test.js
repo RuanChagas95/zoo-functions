4732117ea08b41742297ec1101579439002d3392
Ruan Chagas <ruanchagas95@gmail.com>
2023-06-19 12:20:15 -0300

    test getOpeningHours

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -24,4 +24,8 @@ describe('Testes da função getOpeningHours', () => {
     expect(() => { getOpeningHours('Friday', '09:00-ZM'); }).toThrow(/abbreviation/));
   it('Para os argumentos Sunday e 09:c0-AM deve lançar uma exceção com a mensagem: \'The minutes should represent a number\'', () =>
     expect(() => { getOpeningHours('Sunday', '09:c0-AM'); }).toThrow(/minutes/));
+  it('Para os argumentos Tuesday e 09:60-AM deve lançar uma exceção com a mensagem: \'The minutes must be between 0 and 59\'.', () =>
+    expect(() => { getOpeningHours('Tuesday', '09:60-AM'); }).toThrow(/between/));
+  it('Para os argumentos Monday e 13:00-AM deve lançar uma exceção com a mensagem: \'The hour must be between 0 and 12\'.', () =>
+    expect(() => { getOpeningHours('Tuesday', '13:00-AM'); }).toThrow(/between/));
 });
